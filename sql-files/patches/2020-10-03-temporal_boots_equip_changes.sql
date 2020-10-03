c38391763576da88a9c8767269b70b9966b59b2f
Andie <dev.olivercarlos@gmail.com>
2020-10-03 14:40:37 +0800

    Add SQL file to update DB items and allow Rebel, Kage/Oboro, etc. to wear regular temporal boots.

---
+++ sql-files/patches/2020-10-03-temporal_boots_equip_changes.sql
@@ -0,0 +1,15 @@
+-- This will allow extended classes like rebel taekwon etc to equip temporal boots [Slotted only] since no one will use unslotted shoes anyway.
+
+
+UPDATE `item_db_re` SET `equip_upper` = '63' WHERE (`id` = '22000');
+UPDATE `item_db_re` SET `equip_upper` = '63' WHERE (`id` = '22001');
+UPDATE `item_db_re` SET `equip_upper` = '63' WHERE (`id` = '22002');
+UPDATE `item_db_re` SET `equip_upper` = '63' WHERE (`id` = '22003');
+UPDATE `item_db_re` SET `equip_upper` = '63' WHERE (`id` = '22004');
+UPDATE `item_db_re` SET `equip_upper` = '63' WHERE (`id` = '22005');
+UPDATE `item_db_re` SET `equip_upper` = '63' WHERE (`id` = '22006');
+UPDATE `item_db_re` SET `equip_upper` = '63' WHERE (`id` = '22007');
+UPDATE `item_db_re` SET `equip_upper` = '63' WHERE (`id` = '22008');
+UPDATE `item_db_re` SET `equip_upper` = '63' WHERE (`id` = '22009');
+UPDATE `item_db_re` SET `equip_upper` = '63' WHERE (`id` = '22010');
+UPDATE `item_db_re` SET `equip_upper` = '63' WHERE (`id` = '22011');
