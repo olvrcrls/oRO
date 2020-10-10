4e16820a1ac38c99b15ab65c886757c983861f97
karthik avinash <karthikavinash@gmail.com>
2020-10-10 18:36:10 +0900

    Removes some warnings from script command mail * Remove the warnings for refine and bound options as these aren't mandatory fields.

--- src/map/script.cpp
+++ src/map/script.cpp
@@ -25379,7 +25379,6 @@ BUILDIN_FUNC(mail){
 
 		// Refine
 		if (!script_hasdata(st, 9)) {
-			ShowError("buildin_mail: missing item refine variable at position %d.\n", 9);
 			break;
 		}
 
@@ -25408,7 +25407,6 @@ BUILDIN_FUNC(mail){
 
 		// Bound
 		if (!script_hasdata(st, 10)) {
-			ShowError("buildin_mail: missing item bound variable at position %d.\n", 10);
 			break;
 		}
 
