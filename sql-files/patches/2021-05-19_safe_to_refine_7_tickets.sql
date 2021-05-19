1911e8f9124c18216648b00649c257dd881f6260
Andie <dev.olivercarlos@gmail.com>
2021-05-19 22:51:06 +0800

    Add safe refine to +7 armor and weapon tickets.

---
+++ sql-files/patches/2021-05-19_safe_to_refine_7_tickets.sql
@@ -0,0 +1,2 @@
+INSERT INTO `item_db_re` (`id`, `name_english`, `name_japanese`, `type`, `price_buy`, `weight`) VALUES ('22803', 'Safe_to_7_Equipment_Certificate', 'Safe to 7 Equipment Certificate', '3', '10', '1');
+INSERT INTO `item_db_re` (`id`, `name_english`, `name_japanese`, `type`, `price_buy`, `weight`) VALUES ('22804', 'Safe_to_7_Armor_Certificate', 'Safe to 7 Armor Certificate', '3', '10', '1');
