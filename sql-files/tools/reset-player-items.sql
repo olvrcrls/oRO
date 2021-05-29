57f9e1fd4e9cd0c5850fc1b97e998235672cd7d1
Andie <dev.olivercarlos@gmail.com>
2021-05-29 16:17:59 +0800

    Add tool to reset player items and logs.

---
+++ sql-files/tools/reset-player-items.sql
@@ -0,0 +1,16 @@
+TRUNCATE `inventory`;
+TRUNCATE `guild_storage`;
+TRUNCATE `cart_inventory`;
+TRUNCATE `storage`;
+TRUNCATE `achievement`;
+TRUNCATE `woe_ranking`;
+
+-- Truncate logs
+TRUNCATE `chatlog`;
+TRUNCATE `atcommandlog`;
+TRUNCATE `picklog`;
+TRUNCATE `charlog`;
+TRUNCATE `rank_bg_log`;
+TRUNCATE `rank_woe_log`;
+TRUNCATE `zenylog`;
+TRUNCATE `sc_data`;
