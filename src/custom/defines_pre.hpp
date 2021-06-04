090b13e35073108cc2de1e9ac29ecb97bb9ca8a2
Oliver Carlos <dev.olivercarlos@gmail.com>
2021-06-05 00:48:47 +0800

    Change client from 20180620 to 20180621.

--- src/custom/defines_pre.hpp
+++ src/custom/defines_pre.hpp
@@ -7,7 +7,7 @@
  * rAthena configuration file (http://rathena.org)
  * For detailed guidance on these check http://rathena.org/wiki/SRC/config/
  **/
-#define PACKETVER 20180620
+#define PACKETVER 20180621
 
 
 #endif /* CONFIG_CUSTOM_DEFINES_PRE_HPP */
