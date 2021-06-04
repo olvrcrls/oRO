f564f6664854d870b70d1bc681a0a047aec4a3ab
Oliver Carlos <dev.olivercarlos@gmail.com>
2021-06-04 19:34:28 +0800

    Fix Muscle Fool not being triggered by magic damage.

---
+++ sql-files/patches/06-05-2021_fix_muscle_fool.sql
@@ -0,0 +1,1 @@
+UPDATE `item_db_re` SET `script` = 'autobonus2 \"{ bonus bVit,200; bonus2 bAddRace,RC_All,-50; bonus bMatkRate,-50; }\",10,5000,BF_WEAPON|BF_MAGIC,\"{ specialeffect2 EF_GUARD3; }\";' WHERE (`id` = '4878');
