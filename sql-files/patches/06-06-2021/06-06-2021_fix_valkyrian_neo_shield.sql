ebc225e6885be4f3f7846a3d59821ec0f24c9e6c
Oliver Carlos <dev.olivercarlos@gmail.com>
2021-06-05 18:34:59 +0800

    Fix valkyrian manteau.

---
+++ sql-files/patches/06-06-2021/06-06-2021_fix_valkyrian_neo_shield.sql
@@ -0,0 +1,1 @@
+UPDATE `oro`.`item_db_re` SET `equip_upper` = '63' WHERE (`id` = '2137');
--- sql-files/patches/06-06-2021/06-06-2021_valkyrian_manteau_fix.sql
+++ sql-files/patches/06-06-2021/06-06-2021_valkyrian_manteau_fix.sql
@@ -1,2 +1,3 @@
 UPDATE `item_db_re` SET `view` = '4' WHERE (`id` = '2524');
 UPDATE `item_db_re` SET `equip_jobs` = '4294967295' WHERE (`id` = '2524');
+UPDATE `item_db_re` SET `equip_upper` = '63' WHERE (`id` = '2524');
