c118405feddd85c6a1d45e98c55583cb43fc1835
Oliver Carlos <dev.olivercarlos@gmail.com>
2021-06-10 21:47:15 +0800

    Update SQL file of Muscle Fool fix.

--- sql-files/patches/06-10-2021/06-10-2021_fix_muscle_fool_effect.sql
+++ sql-files/patches/06-10-2021/06-10-2021_fix_muscle_fool_effect.sql
@@ -1,1 +1,2 @@
 UPDATE `item_db_re` SET `unequip_script` = NULL WHERE (`id` = '4878');
+UPDATE `item_db_re` SET `script` = 'autobonus2 \"{ bonus bVit,200; bonus2 bAddRace,RC_All,-50; bonus bMatkRate,-50; }\",50,5000,BF_WEAPON|BF_MAGIC,\"{ specialeffect2 EF_GUARD3; }\";' WHERE (`id` = '4878');
