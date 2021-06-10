31b49a92f1ad58e93cafd1d3e95b75961914deca
Oliver Carlos <dev.olivercarlos@gmail.com>
2021-06-10 18:33:46 +0800

    Comment out the PACKET keys.

--- src/custom/defines_pre.hpp
+++ src/custom/defines_pre.hpp
@@ -8,6 +8,9 @@
  * For detailed guidance on these check http://rathena.org/wiki/SRC/config/
  **/
 #define PACKETVER 20180621
+//#define PACKET_OBFUSCATION_KEY1 0x290551EA
+//#define PACKET_OBFUSCATION_KEY2 0x2B952C75 
+//#define PACKET_OBFUSCATION_KEY3 0x2D67669B
 
 
 #endif /* CONFIG_CUSTOM_DEFINES_PRE_HPP */
