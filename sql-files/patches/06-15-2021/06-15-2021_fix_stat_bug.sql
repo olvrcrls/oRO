603c4a320b3c30c5520cd941efe3413bcf0960bb
Oliver Carlos <dev.olivercarlos@gmail.com>
2021-06-15 22:09:20 +0800

    SQL fix for stat bugs.

--- sql-files/patches/06-15-2021/06-15-2021_fix_stat_bug.sql
+++ sql-files/patches/06-15-2021/06-15-2021_fix_stat_bug.sql
@@ -6,3 +6,13 @@ DELETE FROM `char_reg_num` WHERE `key` LIKE 'VIT_%';
 DELETE FROM `char_reg_num` WHERE `key` LIKE 'INT_%';
 DELETE FROM `char_reg_num` WHERE `key` LIKE 'DEX_%';
 DELETE FROM `char_reg_num` WHERE `key` LIKE 'LUK_%';
+DELETE FROM `char_reg_str` WHERE `key` LIKE 'Slot_%';
+
+-- Default Class update excluding doram
+UPDATE `char` SET `str` = 1, `agi` = 1, `vit` = 1, `int` = 1, `dex` = 1, `luk` = 1, `status_point` = 3330
+WHERE `base_level` = 175 AND class NOT IN (4239, 4240, 4218);
+
+
+-- SE/SR/Rebel update excluding doram
+UPDATE `char` SET `str` = 1, `agi` = 1, `vit` = 1, `int` = 1, `dex` = 1, `luk` = 1, `status_point` = 3278
+WHERE `base_level` = 175 AND class IN (4239, 4240, 4215, 4211, 4212) AND class != 4218;
