5afce88dddeead55da42afd2a1f4e5fd45f64db0
Oliver Carlos <dev.olivercarlos@gmail.com>
2021-06-29 16:42:31 +0800

    Fix the SP bonus if Gate if Hell.

--- src/map/battle.cpp
+++ src/map/battle.cpp
@@ -5640,7 +5640,7 @@ static struct Damage battle_calc_weapon_attack(struct block_list *src, struct bl
 			break;
 		case SR_GATEOFHELL: {
 			struct status_data *sstatus = status_get_status_data(src);
-			int64 bonus = 1 + skill_lv * 2 / 10;
+			int64 bonus = 100 + skill_lv * 20 / 100;
 
 			ATK_ADD(wd.damage, wd.damage2, sstatus->max_hp - sstatus->hp);
 			if(sc && sc->data[SC_COMBO] && sc->data[SC_COMBO]->val1 == SR_FALLENEMPIRE) {
