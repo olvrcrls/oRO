400433ae3dbf3ca948645f806761ba168f760cb9
Oliver Carlos <dev.olivercarlos@gmail.com>
2021-08-01 20:01:36 +0800

    Make GT Revitalize and Change be dispellable.

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -8316,7 +8316,7 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, ui
 					case SC_RECOGNIZEDSPELL:case SC_LEADERSHIP:		case SC_GLORYWOUNDS:
 					case SC_SOULCOLD:		case SC_HAWKEYES:		case SC_REGENERATION:
 					case SC_PUSH_CART:		case SC_RAISINGDRAGON:	case SC_GT_ENERGYGAIN:
-					case SC_GT_CHANGE:		case SC_GT_REVITALIZE:	
+					// case SC_GT_CHANGE:		case SC_GT_REVITALIZE:	
 					case SC_REFLECTDAMAGE:
 					case SC_INSPIRATION:	case SC_EXEEDBREAK:		case SC_FORCEOFVANGUARD:
 					case SC_BANDING:		case SC_DUPLELIGHT:		case SC_EXPIATIO:
