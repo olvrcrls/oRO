778cf650f081dc76482a444ebf7f8c110ef7ac97
Oliver Carlos <dev.olivercarlos@gmail.com>
2021-08-14 00:37:07 +0800

    Set Impositio to have no animation on party members upon cast.

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -7568,10 +7568,15 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, ui
 			party_foreachsamemap(skill_area_sub, sd, skill_get_splash(skill_id, skill_lv), src, skill_id, skill_lv, tick, flag|BCT_PARTY|1, skill_castend_nodamage_id);
 		break;
 
-	case AL_ANGELUS:
 #ifdef RENEWAL
 	case PR_IMPOSITIO:
+		if( sd == NULL || (sd->status.party_id == 0 && !map_getmapflag(sd->bl.m, MF_BATTLEGROUND)) || (flag & 1) )
+			clif_skill_nodamage(src, bl, skill_id, skill_lv, sc_start(src,bl,type,100,skill_lv,skill_get_time(skill_id,skill_lv)));
+		else if( sd )
+			party_foreachsamemap(skill_area_sub, sd, skill_get_splash(skill_id, skill_lv), src, skill_id, skill_lv, tick, flag|BCT_PARTY|1, skill_castend_nodamage_id);
+		break;
 #endif
+	case AL_ANGELUS:
 	case PR_MAGNIFICAT:
 	case PR_GLORIA:
 	case SN_WINDWALK:
